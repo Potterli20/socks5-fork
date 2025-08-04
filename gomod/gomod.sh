bd4707987f3296630e8c4e19351277ba1ad7f5f7
trli <potterli520@gmail.com>
2025-08-04 21:26:56 +0800

    Update gomod.sh

--- gomod/gomod.sh
+++ gomod/gomod.sh
@@ -6,4 +6,4 @@ go get zgo.at/zcache/v2@$zcache_commit_hash
 # 获取 dns 最新的 commits
 dns_commit_hash=$(curl -s https://api.github.com/repos/miekg/dns/commits | grep "sha" | head -n 1 | cut -d '"' -f 4)
 # 使用提取的 commit hash 通过 go get 获取 zcache-go
-go get github.com/miekg/dns@$dns
+go get github.com/miekg/dns@$dns_commit_hash
