9e0776483f9aebd130824057b4e45342722a8d59
trli <potterli520@gmail.com>
2025-08-10 23:45:28 +0800

    Update gomod.sh

--- gomod/gomod.sh
+++ gomod/gomod.sh
@@ -5,5 +5,10 @@ go get github.com/zondax/golem/pkg/zcache@$zcache_commit_hash
 
 # 获取 dns 最新的 commits
 dns_commit_hash=$(curl -s https://api.github.com/repos/miekg/dns/commits | grep "sha" | head -n 1 | cut -d '"' -f 4)
-# 使用提取的 commit hash 通过 go get 获取 zcache-go
+# 使用提取的 commit hash 通过 go get 获取 dns
 go get github.com/miekg/dns@$dns_commit_hash
+
+# 获取 gopsutil 最新的 commits
+gopsutil_commit_hash=$(curl -s https://api.github.com/repos/shirou/gopsutil/commits | grep "sha" | head -n 1 | cut -d '"' -f 4)
+# 使用提取的 commit hash 通过 go get 获取 gopsutil
+go get github.com/shirou/gopsutil@$gopsutil_commit_hash
