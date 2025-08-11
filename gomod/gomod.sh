eb6136d26128e62c1487adf0eb4898349430c630
trli <potterli520@gmail.com>
2025-08-11 20:16:11 +0800

    Update gomod.sh

--- gomod/gomod.sh
+++ gomod/gomod.sh
@@ -7,8 +7,3 @@ go get github.com/Potterli20/golem@$zcache_commit_hash
 dns_commit_hash=$(curl -s https://api.github.com/repos/miekg/dns/commits | grep "sha" | head -n 1 | cut -d '"' -f 4)
 # 使用提取的 commit hash 通过 go get 获取 dns
 go get github.com/miekg/dns@$dns_commit_hash
-
-# 获取 gopsutil 最新的 commits
-gopsutil_commit_hash=$(curl -s https://api.github.com/repos/shirou/gopsutil/commits | grep "sha" | head -n 1 | cut -d '"' -f 4)
-# 使用提取的 commit hash 通过 go get 获取 gopsutil
-go get github.com/shirou/gopsutil/v4@$gopsutil_commit_hash
